6db77908b08c45e62d6058ca35ab163c12d9f9cc
dedy-gunkid <gunkiddedy@gmail.com>
2024-06-29 13:06:05 +0700

    Use MetaMask as the Web3.js Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Web3 } from "web3";
 
 function App() {
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [warning, setWarning] = useState<string | null>(null);
+  const [provider, setProvider] = useState<string | null>(null);
+  const [chainId, setChainId] = useState<string | null>(null);
+  const [latestBlock, setLatestBlock] = useState<string | null>(null);
+
+  useEffect(() => {
+    // ensure that there is an injected the Ethereum provider
+    if (window.ethereum) {
+      // use the injected Ethereum provider to initialize Web3.js
+      setWeb3(new Web3(window.ethereum));
+      // check if Ethereum provider comes from MetaMask
+      if (window.ethereum.isMetaMask) {
+        setProvider("Connected to Ethereum with MetaMask.");
+      } else {
+        setProvider("Non-MetaMask Ethereum provider detected.");
+      }
+    } else {
+      // no Ethereum provider - instruct user to install MetaMask
+      setWarning("Please install MetaMask");
+    }
+  }, []);
+
+  useEffect(() => {
+    async function getChainId() {
+      if (web3 === null) {
+        return;
+      }
+
+      // get chain ID and populate placeholder
+      setChainId(`Chain ID: ${await web3.eth.getChainId()}`);
+    }
+
+    async function getLatestBlock() {
+      if (web3 === null) {
+        return;
+      }
+
+      // get latest block and populate placeholder
+      setLatestBlock(`Latest Block: ${await web3.eth.getBlockNumber()}`);
+
+      // subscribe to new blocks and update UI when a new block is created
+      const blockSubscription = await web3.eth.subscribe("newBlockHeaders");
+      blockSubscription.on("data", (block) => {
+        setLatestBlock(`Latest Block: ${block.number}`);
+      });
+    }
+
+    getChainId();
+    getLatestBlock();
+  }, [web3]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="warn" style={{ color: "red" }}>
+        {warning}
+      </div>
+      <div id="provider">{provider}</div>
+      <div id="chainId">{chainId}</div>
+      <div id="latestBlock">{latestBlock}</div>
+    </>
   );
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+import { MetaMaskProvider } from "web3";
+
+/// <reference types="react-scripts" />
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskProvider;
+  }
+}

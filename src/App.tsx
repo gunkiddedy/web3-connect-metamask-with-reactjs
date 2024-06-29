a1c0ea5ed42f74fac6149b7214dc1a34b492a1d5
dedy-gunkid <gunkiddedy@gmail.com>
2024-06-29 13:29:50 +0700

    Request Access to the MetaMask Accounts

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   const [provider, setProvider] = useState<string | null>(null);
   const [chainId, setChainId] = useState<string | null>(null);
   const [latestBlock, setLatestBlock] = useState<string | null>(null);
-
+  const [accountButtonDisabled, setAccountButtonDisabled] =
+    useState<boolean>(false);
+  const [accounts, setAccounts] = useState<string[] | null>(null);
+  const [connectedAccount, setConnectedAccount] = useState<string | null>(null);
   useEffect(() => {
     // ensure that there is an injected the Ethereum provider
     if (window.ethereum) {
@@ -22,6 +25,7 @@ function App() {
     } else {
       // no Ethereum provider - instruct user to install MetaMask
       setWarning("Please install MetaMask");
+      setAccountButtonDisabled(true);
     }
   }, []);
 
@@ -53,7 +57,23 @@ function App() {
     getChainId();
     getLatestBlock();
   }, [web3]);
-  
+
+  // click event for "Request MetaMask Accounts" button
+  async function requestAccounts() {
+    if (web3 === null) {
+      return;
+    }
+
+    // request accounts from MetaMask
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    document.getElementById("requestAccounts")?.remove();
+
+    // get list of accounts
+    const allAccounts = await web3.eth.getAccounts();
+    setAccounts(allAccounts);
+    // get the first account and populate placeholder
+    setConnectedAccount(`Account: ${allAccounts[0]}`);
+  }
   return (
     <>
       <div id="warn" style={{ color: "red" }}>
@@ -62,6 +82,16 @@ function App() {
       <div id="provider">{provider}</div>
       <div id="chainId">{chainId}</div>
       <div id="latestBlock">{latestBlock}</div>
+      <div id="connectedAccount">{connectedAccount}</div>
+      <div>
+        <button
+          onClick={() => requestAccounts()}
+          id="requestAccounts"
+          disabled={accountButtonDisabled}
+        >
+          Request MetaMask Accounts
+        </button>
+      </div>
     </>
   );
 }
